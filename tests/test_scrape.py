c2fd329f784b976dc9509314bbe7870ab91c7376
Adrian Azoitei <azo.adrian@gmail.com>
2024-02-17 12:16:50 +0100

    allow filtering the query on property type

--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -46,3 +46,39 @@ class TestFundaScraper(object):
         df = preprocess_data(df, is_past=True)
         assert df.shape[0] > 12
         assert df.shape[1] == 17
+
+    def test_buy_house(self):
+        scraper = FundaScraper(area="amsterdam", property_type="house", want_to="buy", find_past=False, page_start=1, n_pages=1)
+        df = scraper.run(raw_data=True)
+        assert len(scraper.links) == 15
+        assert df.shape == (15, 27)
+        assert df['city'].unique()[0] == 'amsterdam'
+
+        df = preprocess_data(df, is_past=False)
+        assert df.shape[0] > 12
+        assert df.shape[1] == 17
+        assert df['house_type'].unique()[0] == 'huis'
+
+    def test_buy_apartment(self):
+        scraper = FundaScraper(area="amsterdam", property_type="apartment", want_to="buy", find_past=False, page_start=1, n_pages=1)
+        df = scraper.run(raw_data=True)
+        assert len(scraper.links) == 15
+        assert df.shape == (15, 27)
+        assert df['city'].unique()[0] == 'amsterdam'
+
+        df = preprocess_data(df, is_past=False)
+        assert df.shape[0] > 12
+        assert df.shape[1] == 17
+        assert df['house_type'].unique()[0] == 'appartement'
+
+    def test_buy_mixed(self):
+        scraper = FundaScraper(area="amsterdam", property_type="apartment,house", want_to="buy", find_past=False, page_start=1, n_pages=1)
+        df = scraper.run(raw_data=True)
+        assert len(scraper.links) == 15
+        assert df.shape == (15, 27)
+        assert df['city'].unique()[0] == 'amsterdam'
+
+        df = preprocess_data(df, is_past=False)
+        assert df.shape[0] > 12
+        assert df.shape[1] == 17
+        assert set(df['house_type'].unique()) == set(["appartement", "huis"])
